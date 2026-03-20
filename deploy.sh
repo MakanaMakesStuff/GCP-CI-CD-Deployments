1805d912566171a609776be16b07e45654787ee9
Makana Edwards <makrigamdeveloping@gmail.com>
2026-03-20 17:10:13 -0400

    Refactor deploy script to eliminate duplicate variable assignments and improve validation for deploy type

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,13 @@
+DEPLOY_TAG=$1
+DEPLOY_TYPE=$2
+
+$DEPLOY_TYPE=${DEPLOY_TYPE:-staging}
+
+if [ "${DEPLOY_TYPE}" != "staging" ] && [ "${DEPLOY_TYPE}" != "production" ]; then
+  echo "Error: Invalid deploy type passed. Usage: npm run deploy <deploy-tag> 'staging'|'production'"
+  exit 1
+fi
+
 # load env file
 ENV_FILE=".env.${DEPLOY_TYPE}"
 if [ ! -f "$ENV_FILE" ]; then
@@ -22,9 +32,6 @@ for VAR in "${REQUIRED_VARS[@]}"
 
 echo "All required env vars are set."
 
-DEPLOY_TAG=$1
-DEPLOY_TYPE=$2
-
 if [ -z "${DEPLOY_TAG}" ]; then
   echo "Error: No deploy tag was provided. Usage: npm run deploy <deploy-tag>"
   exit 1
@@ -34,13 +41,6 @@ echo "Checking if deploy tag already exists in registry..."
 
 if docker manifest inspect "$DOCKER_USERNAME/${DEPLOY_TYPE}:$DEPLOY_TAG" > /dev/null 2>&1; then
   echo "Error: Tag '$DEPLOY_TAG' already exists in the registry. Please update the tag and try again."
-  exit 1
-fi
-
-$DEPLOY_TYPE=${DEPLOY_TYPE:-staging}
-
-if [ "${DEPLOY_TYPE}" != "staging" ] && [ "${DEPLOY_TYPE}" != "production" ]; then
-  echo "Error: Invalid deploy type passed. Usage: npm run deploy <deploy-tag> 'staging'|'production'"
   exit 1
 fi
 
