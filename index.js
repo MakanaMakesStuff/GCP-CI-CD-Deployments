83f4a8269135600e8e03dc0cf9b9cc8c6c1c6375
Makana Edwards <makrigamdeveloping@gmail.com>
2026-03-20 19:45:23 -0400

    Remove commented code for cleaner index.js

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const os = require('os')
 const app = express();
 const port = 3000;
 
-// Simple test route
 app.get("/", (req, res) => {
   // Docker sets the hostname of each replica to the container id by default
   const containerId = os.hostname(); 
   res.send(`Welcome to GCP CI/CD Demo! Running on container: ${containerId}`);
 });
 
-// Listen on 0.0.0.0 so Docker can expose it externally
 app.listen(port, "0.0.0.0", () => {
   console.log(`App listening on port ${port}`);
 });
